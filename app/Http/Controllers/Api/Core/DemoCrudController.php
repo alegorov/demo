e0a45b88edb526b945dbcd6eac266071823a9edd
Alexey Egorov <alegorov@mail.ru>
2025-02-28 16:25:53 +0200

    Add generate and clear

--- app/Http/Controllers/Api/Core/DemoCrudController.php
+++ app/Http/Controllers/Api/Core/DemoCrudController.php
@@ -4,9 +4,26 @@ namespace App\Http\Controllers\Api\Core;
 
 use App\Http\Controllers\Api\ApiCrudController;
 use App\Http\Crud\Core\DemoCrudService;
+use App\Models\Demo;
+use Illuminate\Routing\Router;
 
 class DemoCrudController extends ApiCrudController {
     public function __construct(DemoCrudService $crudService) {
         parent::__construct($crudService);
+    }
+
+    public function routes(Router $router) {
+        parent::routes($router);
+
+        $router->post('generate', [static::class, 'generate']);
+        $router->post('clear', [static::class, 'clear']);
+    }
+
+    public function generate() {
+        Demo::factory(1000)->create();
+    }
+
+    public function clear() {
+        Demo::truncate();
     }
 }
--- frontend/pages/dashboard/Dashboard.vue
+++ frontend/pages/dashboard/Dashboard.vue
@@ -1,6 +1,80 @@
 <template>
-    <div/>
+    <v-container>
+        <v-row>
+            <v-btn
+                :loading="loading"
+                class="ml-4"
+                @click="generate"
+            >
+                Generate 1000
+            </v-btn>
+
+            <v-btn
+                :loading="loading"
+                class="ml-4"
+                @click="clear"
+            >
+                Clear
+            </v-btn>
+        </v-row>
+
+        <v-snackbar
+            v-model="snackbarError"
+            color="red"
+        >
+            {{ snackbarErrorText }}
+        </v-snackbar>
+    </v-container>
 </template>
 
 <script setup lang="ts">
+    import {ref, Ref} from 'vue'
+    import api from '@/api'
+    import {MODEL_INFO} from '@/utils/model-info'
+    import router from '@/router'
+
+    const loading: Ref<boolean> = ref(false)
+    const snackbarError: Ref<boolean> = ref(false)
+    const snackbarErrorText: Ref<string> = ref('')
+
+    async function generate() {
+        if (loading.value) {
+            return
+        }
+
+        loading.value = true
+        try {
+            await api.post(`/${MODEL_INFO.DEMO.ENDPOINT}/generate`)
+            router.push({name: `${MODEL_INFO.DEMO.NAME}-list`})
+        } catch (e) {
+            if (e.response?.data?.message) {
+                snackbarErrorText.value = e.response?.data?.message
+            } else {
+                snackbarErrorText.value = 'Error'
+            }
+            snackbarError.value = true
+        } finally {
+            loading.value = false
+        }
+    }
+
+    async function clear() {
+        if (loading.value) {
+            return
+        }
+
+        loading.value = true
+        try {
+            await api.post(`/${MODEL_INFO.DEMO.ENDPOINT}/clear`)
+        } catch (e) {
+            if (e.response?.data?.message) {
+                snackbarErrorText.value = e.response?.data?.message
+            } else {
+                snackbarErrorText.value = 'Error'
+            }
+            snackbarError.value = true
+        } finally {
+            loading.value = false
+        }
+    }
 </script>
